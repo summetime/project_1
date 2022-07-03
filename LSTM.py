942c18cd0603614f3ca928b57b086fe86c6842e7
summetime <1655325478@qq.com>
2022-07-03 10:32:23 +0800

    LSTM

--- LSTM.py
+++ LSTM.py
@@ -137,7 +137,6 @@ def train(args: Dict):
                     batch = batch.to(device)
                     target = target.to(device)
                 # forward
-                print('cuda: ',cuda)
                 cuda += 1
                 hidden = model.init_hidden(batch.size(0), 1).to(device)
                 cell = hidden
