dc2f12e52f50e3f0f07d39cc5a3f262f324eb95d
summetime <1655325478@qq.com>
2022-07-29 23:01:30 +0800

    train

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ class ScaledDotProductAttention(nn.Module):
     def __init__(self):
         super(ScaledDotProductAttention, self).__init__()
 
-    def forward(self, Q, K, V, d_k, mask, dropout=None):
+    def forward(self, Q, K, V, d_k, mask):
         """
         Q: [batch_size, n_heads, len_q, d_k]
         K: [batch_size, n_heads, len_k, d_k]
@@ -30,7 +30,7 @@ class ScaledDotProductAttention(nn.Module):
         attn_mask: [batch_size, n_heads, seq_len, seq_len]
         """
         s = torch.matmul(Q, K.transpose(-1, -2)) / math.sqrt(d_k)  # scores : [batch_size, n_heads, len_q, len_k]
-        # mask矩阵填充scores（用-1e9填充s中与attn_mask中值为1位置相对应的元素）
+        # mask矩阵填充s（用-1e9填充s中与mask中值为1位置相对应的元素）
         # mask掉那些为了padding长度增加的token，让其通过softmax计算后为0
         if mask is not None:
             s = s.masked_fill(mask == 0, -1e9)
@@ -38,16 +38,13 @@ class ScaledDotProductAttention(nn.Module):
         p = F.softmax(s, dim=-1)  # 对最后一个维度(v)做softmax
         # s : [batch_size, n_heads, len_q, len_k] * V: [batch_size, n_heads, len_v(=len_k), d_v]
         output = torch.matmul(p, V)  # output: [batch_size, n_heads, len_q, d_v]
-        # output：[[z1,z2,...],[...]]向量, p注意力稀疏矩阵（用于可视化的）
-        if dropout is not None:
-            s = dropout(s)
 
         return output
 
 
 class FeedForward(nn.Module):
 
-    def __init__(self, embedding_dim, device, d_ff=2048, dropout=0.1):
+    def __init__(self, embedding_dim, device, d_ff=2048):
         super().__init__()
 
         self.embed = embedding_dim
@@ -57,16 +54,12 @@ class FeedForward(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(embedding_dim, d_ff),
             nn.ReLU(),
-            nn.Dropout(dropout),
             nn.Linear(d_ff, embedding_dim),
         )
 
     def forward(self, input):
         residual = input
         output = self.net(input)
-        # return nn.LayerNorm(d_model).to(device)(output + residual)
-        # n = nn.LayerNorm(self.embed)
-        # output = n(output + residual)
         return nn.LayerNorm(self.embed).to(self.device)(output + residual)  # [bsize, seql, embedding_dim]
 
 
@@ -80,30 +73,24 @@ class Mask():
         encoder和decoder都可能调用这个函数，所以seq_len视情况而定
         seq_q: [bsize, sqel]
         seq_k: [bsize, sqel]
-        seq_len could be src_len or it could be tgt_len
-        seq_len in seq_q and seq_len in seq_k maybe not equal
         """
         bsize, len_q = seq_q.size()  # 这个seq_q只是用来expand维度的
         bsize, len_k = seq_k.size()
-        # eq(zero) is PAD token
-        # 例如:seq_k = [[1,2,3,4,0], [1,2,3,5,0]]
-        mask = seq_k.data.eq(0).unsqueeze(1)  # [batch_size, 1, len_k], True is masked
+        mask = seq_k.data.eq(0).unsqueeze(1)  # [batch_size, 1, len_k], 原来为0的设为true其他false
         return mask.expand(bsize, len_q, len_k)  # [batch_size, len_q, len_k] 构成一个立方体(batch_size个这样的矩阵)
 
     def de_mask(self, seq):
-        """建议打印出来看看是什么的输出（一目了然）
+        """
         seq: [batch_size, tgt_len]
         """
-        attn_shape = [seq.size(0), seq.size(1), seq.size(1)]
-        # attn_shape: [batch_size, tgt_len, tgt_len]
-        subsequence_mask = np.triu(np.ones(attn_shape), k=1)  # 生成一个上三角矩阵
-        subsequence_mask = torch.from_numpy(subsequence_mask).byte()
+        subsequence_mask = np.triu(np.ones((seq.size(0), seq.size(1), seq.size(1))),k=1)  # 生成一个上三角矩阵 [batch_size, tgt_len, tgt_len]
+        subsequence_mask = torch.from_numpy(subsequence_mask).byte()  # 数组换成tensor
         return subsequence_mask  # [batch_size, tgt_len, tgt_len]
 
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, heads, embedding_dim, device,dropout=0.1):
+    def __init__(self, heads, embedding_dim, device):
         super().__init__()
 
         self.embed = embedding_dim  # 512
@@ -116,62 +103,56 @@ class MultiHeadAttention(nn.Module):
         self.k_linear = nn.Linear(embedding_dim, embedding_dim)
         self.attention = ScaledDotProductAttention()
 
-        self.dropout = nn.Dropout(dropout)
         self.out = nn.Linear(embedding_dim, embedding_dim)
 
     def forward(self, q, k, v, mask=None):
         residual, bsize = q, q.size(0)
-        # q:[bsize,nquery,embedding_dim] k:[bsize,sqel,embedding_dim] q:[bsize,sqel,embedding_dim]
-        # perform linear operation and split into N heads
+        # q:[bsize,nquery,embedding_dim] k:[bsize,sqel,embedding_dim] v:[bsize,sqel,embedding_dim]
+        # 将Q K V分为8个部分 分别计算
         k = self.k_linear(k).view(bsize, -1, self.h, self.d_k).transpose(1,2)  # [bsize,nquery,heads,embedding_dim/8] → [bsize,heads,nquery,embedding_dim/8]
         q = self.q_linear(q).view(bsize, -1, self.h, self.d_k).transpose(1,2)  # [bsize,sqel,heads,embedding_dim/8] → [bsize,heads,sqel,embedding_dim/8]
         v = self.v_linear(v).view(bsize, -1, self.h, self.d_k).transpose(1,2)  # [bsize,sqel,heads,embedding_dim/8] → [bsize,heads,sqel,embedding_dim/8]
-        # 因为是多头，所以mask矩阵要扩充成4维的
-        # mask: [bsize, seql, seql] -> [bsize,n_heads, seql, seql]
-        mask = mask.unsqueeze(1).repeat(1, self.h, 1, 1)
+        # mask矩阵扩充成4维
+        # mask: [bsize, seql, seql] -> [bsize, 1, seql, seql]->[bsize,n_heads, seql, seql]
+        mask = mask.unsqueeze(1).repeat(1, self.h, 1, 1) #扩充之后在n_heads出重复填充
 
-        # calculate attention using function we will define next
-        ss = self.attention(q, k, v, self.d_k, mask, self.dropout)
-        # concatenate heads and put through final linear layer
-        concat = ss.transpose(1, 2).contiguous().view(bsize, -1, self.embed)
-        output = self.out(concat)
-        # n = nn.LayerNorm(self.embed)
-        # output = n(output+residual)
+        ss = self.attention(q, k, v, self.d_k, mask) # [batch_size, n_heads, len_q, d_v]
+        # 拼接
+        concat = ss.transpose(1, 2).contiguous().view(bsize, -1, self.embed) #维度转换 → contiguous()拷贝了一份张量在内存中的地址，然后将地址按照形状改变后的张量的语义进行排列
+        output = self.out(concat) #过linear
 
         return nn.LayerNorm(self.embed).to(self.device)(output + residual)
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, embedding_dim,dropout=0.1, max_len=5000):
+    def __init__(self, embedding_dim, dropout=0.1, max_len=256):
         super(PositionalEncoding, self).__init__()
 
         self.dropout = nn.Dropout(p=dropout)
 
         pe = torch.zeros(max_len, embedding_dim)  # 位置编码矩阵，维度[max_len, embedding_dim]
-        position = torch.arange(0.0, max_len).unsqueeze_(1)  # 单词位置
-
-        div_term = torch.exp(torch.arange(0.0, embedding_dim, 2) * (- math.log(1e4) / embedding_dim))  # 使用exp和log实现幂运算
-        div_term.unsqueeze_(0)
-        pe[:, 0:: 2] = torch.sin(torch.mm(position, div_term))  # 计算单词位置沿词向量维度的纹理值
+        position = torch.arange(0.0, max_len).unsqueeze(1)  # 单词位置  [max_len,1]
+        div_term = torch.exp(torch.arange(0.0, embedding_dim, 2) * (- math.log(1e4) / embedding_dim)).unsqueeze(0)  # 使用exp和log实现幂运算 [1,embedding_dim/2]
+        pe[:, 0:: 2] = torch.sin(torch.mm(position, div_term))  # 隔行处理
         pe[:, 1:: 2] = torch.cos(torch.mm(position, div_term))
-        pe.unsqueeze_(0)  # 增加批次维度，[1, max_len, embedding_dim]
+        pe = pe.unsqueeze(0)  # 增加批次维度，[1, max_len, embedding_dim]
         self.register_buffer('pe', pe)  # 将位置编码矩阵注册为buffer(不参加训练)
 
     def forward(self, x):  # 将一个批次中语句所有词向量与位置编码相加
         """
             x: [batch_size,seq_len,embedding_dim]
         """
-        x = x + self.pe[:, x.size(1), :]  # 注意，位置编码不参与训练，因此设置requires_grad=False
+        x = x + self.pe[:, x.size(1), :]
         return self.dropout(x)
 
 
 class EncoderLayer(nn.Module):
 
-    def __init__(self, embedding_dim, heads,device, dropout=0.1):
+    def __init__(self, embedding_dim, heads,device):
         super().__init__()
 
-        self.attn = MultiHeadAttention(heads, embedding_dim, device,dropout=dropout)
-        self.ff = FeedForward(embedding_dim, device,dropout=dropout)
+        self.attn = MultiHeadAttention(heads, embedding_dim, device)
+        self.ff = FeedForward(embedding_dim, device)
 
     def forward(self, input, mask):
         """E
@@ -204,7 +185,7 @@ class Encoder(nn.Module):
             enc_inputs: [bsize, sqel]
         """
         x = self.embed(input)  # [bsize, sqel, embedding_dim]
-        x = self.pe(x)
+        x = self.pe(x)  # 位置加输入
         mask = self.mask.en_mask(input, input)  # [bsize, sqel, sqel]
         for layer in self.layers:
             x = layer(x, mask)  # [bsize, sqel, embedding_dim], mask: [bsize, n_heads, sqel, sqel]
@@ -212,33 +193,31 @@ class Encoder(nn.Module):
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, embedding_dim, heads, device,dropout=0.1):
+    def __init__(self, embedding_dim, heads, device):
         super(DecoderLayer, self).__init__()
-        self.dec_self_attn = MultiHeadAttention(heads, embedding_dim,device, dropout=dropout)
-        self.dec_enc_attn = MultiHeadAttention(heads, embedding_dim, device,dropout=dropout)
-        self.pos_ffn = FeedForward(embedding_dim, device,dropout=dropout)
+        self.self_attn = MultiHeadAttention(heads, embedding_dim,device)
+        self.en_attn = MultiHeadAttention(heads, embedding_dim, device)
+        self.pos_ffn = FeedForward(embedding_dim, device)
 
-    def forward(self, tgt_inputs, src_outputs, dec_self_attn_mask, dec_enc_attn_mask):
+    def forward(self, tgt_inputs, src_outputs, self_attn_mask, attn_mask):
         """
-        tgt_inputs: [batch_size, tgt_len, d_model]
-        src_outputs: [batch_size, src_len, d_model]
-        dec_self_attn_mask: [batch_size, tgt_len, tgt_len]
-        dec_enc_attn_mask: [batch_size, tgt_len, src_len]
+        tgt_inputs: [bsize, tgt_len, embedding] decoder输入
+        src_outputs: [bsize, src_len, embedding] encoder输出
+        self_attn_mask: [bsize, tgt_len, tgt_len]
+        attn_mask: [bsize, tgt_len, src_len]
         """
-        # dec_outputs: [batch_size, tgt_len, d_model], dec_self_attn: [batch_size, n_heads, tgt_len, tgt_len]
-        tgt_inputs= self.dec_self_attn(tgt_inputs, tgt_inputs, tgt_inputs,dec_self_attn_mask)  # 这里的Q,K,V全是Decoder自己的输入
-        # dec_outputs: [batch_size, tgt_len, d_model], dec_enc_attn: [batch_size, h_heads, tgt_len, src_len]
-        tgt_inputs = self.dec_enc_attn(tgt_inputs, src_outputs, src_outputs,dec_enc_attn_mask)  # Attention层的Q(来自decoder) 和 K,V(来自encoder)
-        tgt_inputs = self.pos_ffn(tgt_inputs)  # [batch_size, tgt_len, d_model]
-        return tgt_inputs  # dec_self_attn, dec_enc_attn这两个是为了可视化的
+        outputs= self.self_attn(tgt_inputs, tgt_inputs, tgt_inputs,self_attn_mask)
+        outputs = self.en_attn(outputs, src_outputs, src_outputs,attn_mask)  # Q:decoder  K,V:encoder
+        outputs = self.pos_ffn(outputs)
+        return outputs
 
 
 class Decoder(nn.Module):
     def __init__(self, tgt_vocab_size, embedding_dim, N, heads, dropout,device):
         super(Decoder, self).__init__()
-        self.tgt_emb = nn.Embedding(tgt_vocab_size, embedding_dim)  # Decoder输入的embed词表
-        self.pos_emb = PositionalEncoding(embedding_dim)
-        self.layers = nn.ModuleList([DecoderLayer(embedding_dim, heads,device) for _ in range(N)])  # Decoder的blocks
+        self.tgt_emb = nn.Embedding(tgt_vocab_size, embedding_dim)  # Decoder词表
+        self.pos_emb = PositionalEncoding(embedding_dim, dropout=dropout)
+        self.layers = nn.ModuleList([DecoderLayer(embedding_dim, heads,device) for _ in range(N)])
         self.mask = Mask()
         self.device = device
 
@@ -246,10 +225,10 @@ class Decoder(nn.Module):
         """
         dec_inputs: [batch_size, tgt_len]
         enc_inputs: [batch_size, src_len]
-        enc_outputs: [batch_size, src_len, d_model]   # 用在Encoder-Decoder Attention层
+        enc_outputs: [batch_size, src_len, embedding]   # 用在Encoder-Decoder Attention层
         """
-        dec_outputs = self.tgt_emb(dec_inputs)  # [batch_size, tgt_len, d_model]
-        dec_outputs = self.pos_emb(dec_outputs.transpose(0, 1)).transpose(0, 1).to(self.device)  # [batch_size, tgt_len, d_model]
+        dec_outputs = self.tgt_emb(dec_inputs)  # [batch_size, tgt_len, embedding]
+        dec_outputs = self.pos_emb(dec_outputs).to(self.device)  # [batch_size, tgt_len, embedding]
         # Decoder输入序列的pad mask矩阵（这个例子中decoder是没有加pad的，实际应用中都是有pad填充的）
         dec_self_attn_pad_mask = self.mask.en_mask(dec_inputs, dec_inputs).to(self.device)  # [batch_size, tgt_len, tgt_len]
         # Masked Self_Attention：当前时刻是看不到未来的信息的
@@ -388,14 +367,12 @@ def train(args: Dict):
                 out = model(en_src, de_src)
                 loss = Loss(out.transpose(1,2), target)
                 bleu_score += bleu(target, out, device)
-                if cuda % 10 == 0:
-                    print('This is bleu_score:',bleu_score)
-                    bleu_score = 0
-                # backward
                 loss.backward()
                 if cuda % 10 == 0:
                     optimizer.step()  # 更新所有参数
                     optimizer.zero_grad()  # 将模型的参数梯度初始化为0
+                    print('This is bleu_score:', bleu_score) #计算bleu值
+                    bleu_score = 0
                 if cuda % 100 == 0:  # 打印loss
                     print("This is {0} epoch,This is {1} batch".format(epoch, cuda), 'loss = ',
                           '{:.6f}'.format(loss / nword_en),'bleu_score=',bleu_score)
@@ -408,11 +385,14 @@ def train(args: Dict):
 def bleu(reference,candidate,device):
     # reference = reference.to(device)
     # candidate = candidate.to(device)
-    reference = [reference.cpu().detach().numpy().tolist()]
+    score = 0
+    reference = reference.cpu().detach().numpy().tolist()
     print(reference)
     candidate = candidate.cpu().detach().numpy().tolist()
+    l = len(candidate)
     print(candidate)
-    score = sentence_bleu(reference, candidate,weights=[0.25,0.25,0.25,0.25]).to(device)
+    for i in range(l):
+        score += sentence_bleu([reference[i]], candidate[i],weights=[0.25,0.25,0.25,0.25]).to(device)
     return score
 
 
@@ -424,3 +404,5 @@ if __name__ == "__main__":
         train(args)
     else:
         raise RuntimeError('invalid run mode')
+
+# python train.py --cuda=0 train --en="commoncrawl_result_de.hdf5" --de="commoncrawl_result_en.hdf5" --target="commoncrawl_result_target.hdf5" --model_save_path="model.trans" --embedding_dim=512 --N=6 --heads=8 --dropout=0.1
